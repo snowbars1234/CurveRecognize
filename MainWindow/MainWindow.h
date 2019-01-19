336600b55f6f26bb763741349276ce8c24db1ec3
Pavel Avdiienko <snowbars1234@gmail.com>
2019-01-19 11:16:34 +0200

    first comit

---
+++ MainWindow/MainWindow.h
@@ -0,0 +1,32 @@
+
+// MainWindow.h : main header file for the PROJECT_NAME application
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'stdafx.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// main symbols
+
+
+// CMainWindowApp:
+// See MainWindow.cpp for the implementation of this class
+//
+
+class CMainWindowApp : public CWinApp
+{
+public:
+	CMainWindowApp();
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+
+// Implementation
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CMainWindowApp theApp;
